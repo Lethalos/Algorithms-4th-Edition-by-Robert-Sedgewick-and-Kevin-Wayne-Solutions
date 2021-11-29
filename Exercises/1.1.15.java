ca14c7e37e099832af5025a1feaf8fb3d8fdb3ca
Kaan Ozdogan <48806820+Lethalos@users.noreply.github.com>
2021-11-29 19:56:33 +0300

    Add files via upload

---
+++ Exercises/1.1.15.java
@@ -0,0 +1,26 @@
+//Exercise 1.1.15
+public class Practice {
+	public static int[] histogram(int[] a, int M) {
+		int[] b = new int[M];
+
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < M; j++) {
+				if (i == a[j]) {
+					b[i]++;
+				}
+			}
+		}
+
+		return b;
+	}
+
+	public static void main(String[] args) {
+		int[] a = { 0, 0, 3, 4, 5, 0, 7, 8, 8, 9};
+
+		int b[] = histogram(a, 10);
+
+		for (int i = 0; i < 10; i++) {
+			System.out.print(b[i] + " ");
+		}
+	}
+}
